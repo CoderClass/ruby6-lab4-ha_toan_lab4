4cbad312b58eaacc1b3dfecc96e2c83c179fead3
hata <hata@tinypulse.com>
2017-07-05 20:19:55 +0700

    Milestone 2: Basic Chat

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,27 @@
+class MessagesController < ApplicationController
+
+  before_action :room
+
+  def index
+  end
+
+  def create
+    @message = @room.messages.build(message_param)
+    if @message.save
+      flash[:success] = "Create message success"
+      redirect_to room_messages_path(@room)
+    else
+      flash.now[:error] = "Cannot create message"
+      render "index"
+    end
+  end
+
+  private
+  def room
+    @room = Room.find(params[:room_id])
+  end
+
+  def message_param
+    params.require(:message).permit(:username, :content)
+  end
+end
