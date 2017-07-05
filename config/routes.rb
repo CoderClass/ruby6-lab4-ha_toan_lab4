cd358aec54dea2d047806d5d3ef622a9408bd49d
Toan Nguyen <n.minhtoan@gmail.com>
2017-07-05 20:21:29 +0700

    login

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     resources :messages
   end
   root 'home#index'
+  post 'login', to: 'home#login'
+  delete 'logout', to: 'home#logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
